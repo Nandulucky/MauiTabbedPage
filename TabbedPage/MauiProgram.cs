d65b4c06f3bfafcc4751182b0d5e20427f7c400d
Nandulucky <Nandulucky@users.noreply.github.com>
2024-11-21 11:07:16 +0530

    Add files via upload

---
+++ TabbedPage/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using TabbedPageSample.Platforms.Android;
+
+namespace TabbedPageSample;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("fa_solid.ttf", "FontAwesome");
+            })
+            .ConfigureMauiHandlers(handlers => {
+#if ANDROID
+                // Add a handler
+                handlers.AddHandler(typeof(TabbedPage), typeof(CustomTabbedPageHandler));
+#endif
+            });
+
+        return builder.Build();
+	}
+}
---
+++ TabbedPage/Platforms/Android/CustomTabbedPageHandler.cs
@@ -0,0 +1,26 @@
+﻿using AndroidX.Fragment.App;
+using Google.Android.Material.Tabs;
+using Microsoft.Maui.Controls.Compatibility.Platform.Android;
+using Microsoft.Maui.Handlers;
+using Platform = Microsoft.Maui.ApplicationModel.Platform;
+
+namespace TabbedPageSample.Platforms.Android;
+
+public class CustomTabbedPageHandler : TabbedViewHandler
+{
+    public override void UpdateValue(string property)
+    {
+        base.UpdateValue(property);
+
+        var tabs = Platform.CurrentActivity?.FindViewById<FragmentContainerView>(Microsoft.Maui.Resource.Id
+            .navigationlayout_toptabs);
+
+        var tabLayout = tabs?.GetChildAt(0);
+
+        if (tabLayout is TabLayout layout)
+        {
+            layout.TabMode = TabLayout.ModeScrollable;
+            layout.SetSelectedTabIndicatorColor(Colors.White.ToAndroid());
+        }
+    }
+}
